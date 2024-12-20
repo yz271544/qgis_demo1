831ffba038717083eeafc98896dfe053cf01df8c
Lyndon.Hu <lyndon.hu9@gmail.com>
2024-12-20 22:06:03 +0800

    success 3d?

--- main3d1.cpp
+++ main3d1.cpp
@@ -4,6 +4,8 @@
 #include <QUrl>
 #include <QUrlQuery>
 #include <QFileInfo>
+#include <qgstiledscenelayer.h>
+#include <qgstiledscenelayer3drenderer.h>
 #include <qgsapplication.h>
 #include <qgsproject.h>
 #include <qgsrasterlayer.h>
@@ -81,15 +83,15 @@ int main(int argc, char *argv[])
     }
     // 将图层添加到项目中
     project->addMapLayer(baseXyzLayer);
-
+    qDebug() << "add base layer to project";
 
     // 创建 XYZ 图层 (使用公开的 OpenStreetMap URL)
     QString urlString = "http://47.94.145.6/map/orthogonal/1847168269595754497-健康谷正射";
 
-    QString encoded_url_string = encodeUrl(urlString);
-    qDebug() << "encoded_url_string: " << encoded_url_string;
+    // QString encoded_url_string = encodeUrl(urlString);
+    // qDebug() << "encoded_url_string: " << encoded_url_string;
     // QString encodedOrthogonalXyzUrl = "type=xyz&url=http://47.94.145.6" + prefix +  partStr + "/{z}/{x}/{y}.png";
-    QString encodedOrthogonalXyzUrl = "type=xyz&url=" + encoded_url_string + "/{z}/{x}/{y}.png";
+    QString encodedOrthogonalXyzUrl = "type=xyz&url=" + urlString + "/{z}/{x}/{y}.png";
     qDebug() << "encodedOrthogonalXyzUrl: " << encodedOrthogonalXyzUrl;
     //QString xyzUrl = "type=xyz&http://tile.openstreetmap.org/{z}/{x}/{y}.png&zmax=19&zmin=0";
     QString orthogonalLayerName = "OrthogonalMap";
@@ -101,7 +103,20 @@ int main(int argc, char *argv[])
     }
     // 将图层添加到项目中
     project->addMapLayer(orthogonalLayer);
+    qDebug() << "add orthogonal layer to project";
 
+    // 3D Scene
+    // QString realistic3d_tile_url = "url=http://47.94.145.6/map/realistic3d/1847168269595754497-jkg/tileset.json&http-header:referer=";
+    QString realistic3d_tile_url = "url=http://47.94.145.6/map/realistic3d/1847168269595754497-jkg/tileset.json";
+    QgsTiledSceneLayer *tiled_scene_layer = new QgsTiledSceneLayer(realistic3d_tile_url, "Real3DTile", "cesiumtiles");
+    tiled_scene_layer->setRenderer3D(new QgsTiledSceneLayer3DRenderer());
+    if (!tiled_scene_layer->isValid()) {
+        qWarning() << "cesiumtiles 3D tiled_scene_layer 图层无效!" << tiled_scene_layer->error().message();
+        return -1;
+    }
+    // 将图层添加到项目中
+    project->addMapLayer(tiled_scene_layer);
+    qDebug() << "add 3d scene layer to project";
 
     qDebug() << "qgisProjectPath:" << qgisProjectPath;
     // 保存项目为 .qgz 文件
