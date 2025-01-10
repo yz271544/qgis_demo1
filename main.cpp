dc59d27c5760b95ad87f4b9ecf38d6b0e8d866b6
Lyndon.Hu <Lyndon.Hu9@gmail.com>
2025-01-10 10:18:40 +0800

    test: 3d view

--- main.cpp
+++ main.cpp
@@ -15,6 +15,9 @@
 #include <QDebug>  // For logging
 #include <qgspluginlayerregistry.h>
 #include <QProcess>
+#include <QTextCodec>
+
+#include "config.h"
 
 #if defined(_WIN32)
 #include <windows.h>
@@ -24,85 +27,48 @@
 
 int main(int argc, char* argv[])
 {
-	QString qgis_source;
-	char* qgissource = std::getenv("QGISSOURCE");
-	if (qgissource != nullptr) {
-		qgis_source = QString::fromStdString(qgissource);
-	}
-	else {
-		qgis_source = "";
-	}
-
-	qDebug() << "qgis_source:" << qgis_source;
+#if defined(_WIN32)
+	// 设置控制台输出为UTF - 8
+	SetConsoleOutputCP(CP_UTF8);
+	QLocale::setDefault(QLocale(QLocale::Chinese, QLocale::China));
+	QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+#endif
 
 	// Initialize QGIS application without graphical interface
-	QgsApplication app(argc, argv, false);
-	QgsApplication::setPrefixPath("/lyndon/iProject/cpath/QGIS/output", true);
-	QgsApplication::setPluginPath("/lyndon/iProject/cpath/QGIS/output/lib/qgis/plugins");
+	QgsApplication app(argc, argv, true);
+	QgsApplication::setPrefixPath(QGIS_PREFIX_PATH, true);
+	//QgsApplication::setPrefixPath("/usr", true);
+	QgsApplication::init();
 	QgsApplication::initQgis();
 
 
-	QgsPluginLayerRegistry* plugin_layer_registry = QgsApplication::pluginLayerRegistry();
-	if (plugin_layer_registry) {
-		plugin_layer_registry->addPluginLayerType(new QgsPluginLayerType(QString::fromStdString("xyzvectortiles")));
-		QStringList plugin_layer_types = plugin_layer_registry->pluginLayerTypes();
-		qDebug() << "plugin type:" << plugin_layer_types.join(", ");
+	qDebug() << "Plugin path: " << QgsApplication::pluginPath();
+	QString save_qgis_project_path = QString(QGIS_PROJECT_PATH);
 
-		QgsPluginLayerType* plugin_layer_type = plugin_layer_registry->pluginLayerType("xyzvectortiles");
-		if (plugin_layer_type) {
-			qDebug() << "XYZ plugin type loaded successfully";
-		}
-		else {
-			qDebug() << "Failed to load XYZ plugin type";
-		}
-	}
-	else {
-		qDebug() << "Failed to load plugin registry";
-	}
-
+	// create QGIS project
 	QgsProject* project = QgsProject::instance();
-	qInfo() << "project instance: " << project;
-	project->setTitle("My QGIS Project");
-	qInfo() << "set title: " << project->title();
-	QgsCoordinateReferenceSystem crs = QgsCoordinateReferenceSystem::fromEpsgId(3857);
-	project->setCrs(crs);  // Set CRS to EPSG:3857
-	qInfo() << "set crs: " << project->crs().authid();
+	project->removeAll();
+	QString crs = "EPSG:3857";
+	QgsCoordinateReferenceSystem qgscrs;
+	qgscrs.createFromString(crs);
+	project->setCrs(qgscrs);
 
-	std::string MAP_BASE_URL;
-	char* env_url = std::getenv("MAP_BASE_URL");
-	if (env_url != nullptr) {
-		MAP_BASE_URL = env_url;
+	// create XYZ base layer
+	QString baseXyzUrl = "type=xyz&url=http://47.94.145.6/map/lx/{z}/{x}/{y}.png&zmax=19&zmin=0";
+	// QString baseXyzUrl = "type=xyz&url=http://172.31.100.34:38083/map/lx/{z}/{x}/{y}.png&zmax=17&zmin=0";
+	//QString baseXyzUrl = "type=xyz&http://tile.openstreetmap.org/{z}/{x}/{y}.png&zmax=19&zmin=0";
+
+	QgsRasterLayer* baseXyzLayer = new QgsRasterLayer(baseXyzUrl, BASE_TILE_NAME, "wms");
+	if (!baseXyzLayer->isValid()) {
+		qWarning() << "XYZ xyzLayer layer invalid!" << baseXyzLayer->error().message();
+		return -1;
 	}
-	else {
-		MAP_BASE_URL = "http://47.94.145.6";
-	}
-
-	// Define WMS layer URL and name
-	std::string BASE_TILE_URL = MAP_BASE_URL + "/map/lx/{z}/{x}/{y}.png";
-	QString baseTileUrl = QString::fromStdString(BASE_TILE_URL);
-	QString baseTileName = "BaseTile";
-
-	qInfo() << "baseTileUrl: " << baseTileUrl;
-	qInfo() << "baseTileName: " << baseTileName;
-
-	// 构建完整的数据源URI字符串
-	QString dataSource = QString("type=xyz&url=%1").arg(baseTileUrl);
-	qDebug() << "dataSource:" << dataSource;
-	// 创建XYZ Raster图层
-	auto* baseTileLayer = new QgsRasterLayer(dataSource, baseTileName, "xyz");
-	baseTileLayer->setCrs(crs);
-
-	// Check if the layer is valid
-	if (baseTileLayer->isValid()) {
-		project->addMapLayer(baseTileLayer);
-	}
-	else {
-		qCritical() << "Error adding XYZ layer: " << baseTileLayer->error().message();
-	}
-
+	// add base layer to project
+	project->addMapLayer(baseXyzLayer);
+	qDebug() << "add base layer to project";
 
 	// 指定保存的QGS文件路径
-	QString projectFilePath = "/lyndon/iProject/cpath/qgis_demo1/common/project/project.qgz";
+	QString projectFilePath = "../common/project/project.qgz";
 	QString tempQgsFilePath = QFileInfo(projectFilePath).absolutePath() + "/temp_project.qgs";
 	qDebug() << "save qgs file:" << tempQgsFilePath;
 	project->write(tempQgsFilePath);
@@ -116,13 +82,13 @@ int main(int argc, char* argv[])
 	zipProcess.start(zipCommand, arguments);
 	zipProcess.waitForFinished(-1);
 
-	if (zipProcess.exitStatus() == QProcess::NormalExit && zipProcess.exitCode() == 0) {
-		qDebug() << "Successfully created QGZ file:" << projectFilePath;
-		QFile::remove(tempQgsFilePath);
-	}
-	else {
-		qCritical() << "Error creating QGZ file";
-	}
+	// if (zipProcess.exitStatus() == QProcess::NormalExit && zipProcess.exitCode() == 0) {
+	// 	qDebug() << "Successfully created QGZ file:" << projectFilePath;
+	// 	QFile::remove(tempQgsFilePath);
+	// }
+	// else {
+	// 	qCritical() << "Error creating QGZ file";
+	// }
 
 	QgsApplication::exitQgis();
 	app.exit();
