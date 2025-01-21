56a00955961e7d9f6163173808f2886bc87e3f0e
Lyndon.Hu <Lyndon.Hu9@gmail.com>
2025-01-21 15:36:31 +0800

    feat: capture image

--- main3d1.cpp
+++ main3d1.cpp
@@ -420,9 +420,18 @@ int main(int argc, char* argv[]) {
 	qDebug() << "save image path: " << capture_scene_image_path;
 	// 创建辅助对象并捕获图像
 	ImageCaptureHelper* helper = new ImageCaptureHelper(qgs_offscreen_3d_engine, qgs_3d_map_scene, capture_scene_image_path);
-	qDebug() << QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss") << " start capture image";
-	helper->captureImage();
-	qDebug() << QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss") << " capture image done";
+
+	QObject::connect(qgs_3d_map_scene, &Qgs3DMapScene::sceneStateChanged, [helper, qgs_3d_map_scene]() {
+		if (qgs_3d_map_scene->sceneState() == Qgs3DMapScene::Ready) {
+			qDebug() << QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss") << " start capture image";
+			helper->captureImage();
+			qDebug() << QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss") << " capture image done";
+		}
+	});
+
+	// qDebug() << QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss") << " start capture image";
+	// helper->captureImage();
+	// qDebug() << QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss") << " capture image done";
 
 	// QImage capture_scene_image = Qgs3DUtils::captureSceneImage(*qgs_offscreen_3d_engine, qgs_3d_map_scene);
 	// qDebug() << "capture_scene_image size:" << capture_scene_image.size();
